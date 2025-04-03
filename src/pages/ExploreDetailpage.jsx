a5b8f61932bc06e350d840f44c6a29e6807f780e
Khan <khan.develop.01@gmail.com>
2025-04-03 20:41:31 +0200

    Pages functions to respective pages

--- src/pages/ExploreDetailpage.jsx
+++ src/pages/ExploreDetailpage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function ExploreDetailpage() {
-  return <div>ExploreDetailpage</div>;
+function ImageDetailPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Image Detail Page</h1>
+      <Link to="/explore" className="bg-gray-200 px-4 py-2 rounded">
+        Back to Explore
+      </Link>
+    </div>
+  );
 }
 
-export default ExploreDetailpage;
+export default ImageDetailPage;
