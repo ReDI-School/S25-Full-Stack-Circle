5288fef3183c228cd563534747545eaf88971cea
Marline John <marlinejohn@gmail.com>
2025-04-03 17:52:15 +0200

    Initial commit

--- src/components/PinDetailComponent.jsx
+++ src/components/PinDetailComponent.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
 function PinDetailComponent() {
+  // dummy data
+  const pin = {
+    link: "https://www.instagram.com/",
+    postName: "Character Design",
+    description: "@inktober day 16: Grungy digital inking using @procreate",
+    hashtags: ["#inktober", "#inktober24", "#inktober2024", "#day16", "#inktoberday16", "#inktobergrungy", "#grungy", "#inking", "#inkart", "digital", "...more"],
+    username: "inscapia",
+    userProfile: "https://placehold.co/50x50", // Placeholder 
+  };
+
   return (
-    <div></div>
+    <div>
+      {/* link */}
+      <a href={pin.link} target="_blank" rel="noopener noreferrer"> 
+        Instagram
+      </a>
+
+       {/* Post Name */}
+       <h2>{pin.postName}</h2>
+
+        {/* Description */}
+      <p>{pin.description}</p>
+
+       {/* Hashtags */}
+       <p>{pin.hashtags.join(" ")}</p>
+
+       {/* User Info (Profile Picture + Username) */}
+      <div style={{ display: "flex", alignItems: "left" }}>
+        <img
+          src={pin.userProfile}
+          alt="User"
+          style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+        />
+        <span>{pin.username}</span>
+      </div>
+
+    </div>
   )
 }
 
