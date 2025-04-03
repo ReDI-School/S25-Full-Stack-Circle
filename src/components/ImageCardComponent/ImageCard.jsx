d3fc313bc385ff3140d3451652689be1e4da5f3e
Oluwabusola <jibodu.busola@gmail.com>
2025-04-03 19:12:37 +0200

    initial commit

---
+++ src/components/ImageCardComponent/ImageCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./ImageCard.css";
+
+function ImageCard() {
+  return (
+    <>
+      <div className="image-card">
+        <p>Image Card on the left</p>
+      </div>
+    </>
+  );
+}
+
+export default ImageCard;
