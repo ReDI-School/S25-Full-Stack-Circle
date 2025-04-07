f60e8dc3af6e3e45677ab87980ea43b7981269c4
Oluwabusola <jibodu.busola@gmail.com>
2025-04-07 11:32:59 +0200

    added imagecard to the detail page

--- src/pages/ImageDetailPage.jsx
+++ src/pages/ImageDetailPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ImageCard from "../components/ImageCardComponent/ImageCard";
 
 function ImageDetailPage() {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold mb-4">Image Detail Page</h1>
+      <ImageCard />
       <Link to="/explore" className="bg-gray-200 px-4 py-2 rounded">
         Back to Explore
       </Link>
