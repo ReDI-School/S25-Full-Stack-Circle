5bad294369829fc3d932d51d7fd624f6444f74df
Oluwabusola <jibodu.busola@gmail.com>
2025-04-07 11:34:18 +0200

    implement the imagecard and styling

--- src/components/ImageCardComponent/ImageCard.jsx
+++ src/components/ImageCardComponent/ImageCard.jsx
@@ -5,19 +5,17 @@ import "./ImageCard.css";
 function ImageCard() {
   return (
     <>
-      <div className="image-card">
-        {/*
-  <p>Image Card on the left</p>
-  */}
-        <img className="image" src="" height={500} width={600} alt="" />
-        <div className="buttons">
-          <button>
-            enlarge button
-            <span>enlarge</span>
-          </button>
-          <button>
-            <span>explore</span>
-          </button>
+      <div className="wrapper">
+        <div className="image-card">
+          <img
+            className="image"
+            src="https://i.pinimg.com/736x/21/fd/0f/21fd0f5ef475dcdd2764425327c0ee68.jpg"
+            alt=""
+          />
+          <div className="button">
+            <button>View larger </button>
+            <button>Explore</button>
+          </div>
         </div>
       </div>
     </>
