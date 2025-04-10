1bc798619dc14a29c0129a19010b1eaf0d1af226
Oluwabusola <jibodu.busola@gmail.com>
2025-04-10 16:52:42 +0200

    implemented react icons

--- src/components/ImageCardComponent/ImageCard.jsx
+++ src/components/ImageCardComponent/ImageCard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./ImageCard.css";
 import ExpandIcon from "@mui/icons-material/Expand";
+import { ImEnlarge2 } from "react-icons/im";
+import { GrSearchAdvanced } from "react-icons/gr";
 
 function ImageCard() {
   return (
@@ -12,13 +14,13 @@ function ImageCard() {
             src="https://i.pinimg.com/736x/21/fd/0f/21fd0f5ef475dcdd2764425327c0ee68.jpg"
             alt=""
           />
-          <div className="button">
-            <button>
-              View larger
-              {/* <ExpandIcon /> */}
-            </button>
-
-            <button>Explore</button>
+          <div className="buttons-div">
+            <div className="button">
+              <ImEnlarge2 />
+            </div>
+            <div className="button">
+              <GrSearchAdvanced />
+            </div>
           </div>
         </div>
       </div>
