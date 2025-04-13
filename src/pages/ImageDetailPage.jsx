29d6b94aa8ed70a567feba73fa95f446c042a290
Khan <khan.develop.01@gmail.com>
2025-04-13 16:22:33 +0200

    addtion of Profile popup and Save Button

--- src/pages/ImageDetailPage.jsx
+++ src/pages/ImageDetailPage.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShopItem from './ImageDetailPage/ShopItem';
-import Breadcrumb from './ImageDetailPage/Breadcrumb'; //
+import Breadcrumb from './ImageDetailPage/Breadcrumb';
 
 function ImageDetailPage() {
   return (
-    <div className="p-4">
-      {/* Breadcrumb Component*/}
+    <div className="pt-24"> {/**/}
       <Breadcrumb />
 
-      <h1 className="text-xl font-bold mb-4">Image Detail Page</h1>
+      <h1 className="text-xl font-bold mb-4"style={{marginTop:"50px",}} >Image Detail Page</h1>
 
-
-      <Link to="/explore" className="bg-gray-200 px-4 py-2 rounded">
-        Back to Explore
+      <Link to="/explore" className="text-red-600 underline font-medium mb-4 inline-block">
+        ← Back to Explore
       </Link>
 
-      {/* ShopItem component*/}
-      <div className="mt-6">
+      <div className="mt-6 flex justify-center">
         <ShopItem />
       </div>
     </div>
