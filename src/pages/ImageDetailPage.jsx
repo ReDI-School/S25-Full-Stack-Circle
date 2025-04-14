80356cdfe2e1cdacf99f345d11c6ae3816c002ff
Julaaaa <julita.lapinska@shapescape.com>
2025-04-14 21:26:38 +0200

    Comment Section component with the style to CSS module

--- src/pages/ImageDetailPage.jsx
+++ src/pages/ImageDetailPage.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import CommentSection from "../components/CommentSection";
-import ShopItem from './ImageDetailPage/ShopItem';
-import Breadcrumb from './ImageDetailPage/Breadcrumb';
-
+import CommentSection from "./ImageDetailPage/CommentSection";
+import ShopItem from "./ImageDetailPage/ShopItem";
+import Breadcrumb from "./ImageDetailPage/Breadcrumb";
 
 function ImageDetailPage() {
-  return (
-    <div className="pt-24"> {/**/}
-      <Breadcrumb />
-
-      <h1 className="text-xl font-bold mb-4"style={{marginTop:"50px",}} >Image Detail Page</h1>
-
-      <Link to="/explore" className="text-red-600 underline font-medium mb-4 inline-block">
-        ← Back to Explore
-      </Link>
-
-      <CommentSection />
-          
-      {/* ShopItem component*/}
-
-      <div className="mt-6 flex justify-center">
-
-        <ShopItem />
-      </div>
-
-    </div>
-  );
+	return (
+		<div className="pt-24">
+			{" "}
+			{/**/}
+			<Breadcrumb />
+			<h1 className="text-xl font-bold mb-4" style={{ marginTop: "50px" }}>
+				Image Detail Page
+			</h1>
+			<Link
+				to="/explore"
+				className="text-red-600 underline font-medium mb-4 inline-block">
+				← Back to Explore
+			</Link>
+			<CommentSection />
+			{/* ShopItem component*/}
+			<div className="mt-6 flex justify-center">
+				<ShopItem />
+			</div>
+		</div>
+	);
 }
 
 export default ImageDetailPage;
