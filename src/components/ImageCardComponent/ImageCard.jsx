06a3469642d641376ea80ac458dc54135277a2c5
Oluwabusola <jibodu.busola@gmail.com>
2025-04-15 10:38:53 +0100

    changed styling to css modules

--- src/components/ImageCardComponent/ImageCard.jsx
+++ src/components/ImageCardComponent/ImageCard.jsx
@@ -1,24 +1,23 @@
-import React, { useEffect, useState } from "react";
-import "./ImageCard.css";
-import ExpandIcon from "@mui/icons-material/Expand";
+import React from "react";
+import styles from "./ImageCard.module.css";
 import { ImEnlarge2 } from "react-icons/im";
 import { GrSearchAdvanced } from "react-icons/gr";
 
 function ImageCard() {
   return (
     <>
-      <div className="wrapper">
-        <div className="image-card">
+      <div className={styles.wrapper}>
+        <div className={styles["image-card"]}>
           <img
-            className="image"
+            className={styles.image}
             src="https://i.pinimg.com/736x/21/fd/0f/21fd0f5ef475dcdd2764425327c0ee68.jpg"
             alt=""
           />
-          <div className="buttons-div">
-            <div className="button">
+          <div className={styles["buttons-div"]}>
+            <div className={styles.button}>
               <ImEnlarge2 />
             </div>
-            <div className="button">
+            <div className={styles.button}>
               <GrSearchAdvanced />
             </div>
           </div>
