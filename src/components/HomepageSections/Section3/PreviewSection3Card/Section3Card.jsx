869b6fe4ef4ad0e3e4f4de7385ad0d4cbd293a09
NavyaBharathan <navyabmanikkoth@live.com>
2025-04-17 17:50:41 +0200

    changed css files to module.css

---
+++ src/components/HomepageSections/Section3/PreviewSection3Card/Section3Card.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Section3Card.module.css'
+
+import cardImage from "../../../../assets/preview-section/Section3Card.jpg";
+import profilePicture from "../../../../assets/preview-section/section3ProfilePicture.jpg";
+
+import { MdKeyboardArrowLeft } from "react-icons/md"
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { BsDashLg } from "react-icons/bs";
+
+const Section3Card = () => {
+  return (
+    <div className={styles['section3-card-wrapper']}>
+      <div className={styles['card-caraousel-wrapper']}> 
+        <img className={styles['card-image']} src={cardImage} />
+        <div className={styles['card-caption-wrapper']}>
+          <div className={styles['carousel-icons']}>
+             <MdKeyboardArrowLeft /><BsDashLg/><BsDashLg/><BsDashLg/><BsDashLg/><BsDashLg/><BiDotsHorizontalRounded/>
+          </div>
+          <div className={styles['card-caption']}>
+            <p >How to find the perfect lipshade for any occassion</p>
+            </div>
+        </div>
+        <div className={styles['card-text']}>
+           <p className={styles['profile-name']}>Scout the city</p>
+           <p className={styles['followers']}>56.7 followers</p>
+        </div>
+      </div>
+      
+         <img  className='profile-picture' src={profilePicture} />
+      
+    </div>
+  )
+}
+
+export default Section3Card
