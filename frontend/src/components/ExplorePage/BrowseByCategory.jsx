30850d8ac5a478229a7a35a77082446fe9040347
AdDON <pdon.wus@gmail.com>
2025-04-28 19:04:43 +0200

    update BrowseByCategory.jsx follow ESlint

--- frontend/src/components/ExplorePage/BrowseByCategory.jsx
+++ frontend/src/components/ExplorePage/BrowseByCategory.jsx
@@ -6,67 +6,67 @@ const categories = [
   {
     id: 1,
     title: "Animals",
-    image: "https://picsum.photos/300/150?random=1",
+    image: "https://picsum.photos/300/150?random=1"
   },
   {
     id: 2,
     title: "Art",
-    image: "https://picsum.photos/300/150?random=2",
+    image: "https://picsum.photos/300/150?random=2"
   },
   {
     id: 3,
     title: "Beauty",
-    image: "https://picsum.photos/300/150?random=3",
+    image: "https://picsum.photos/300/150?random=3"
   },
   {
     id: 4,
     title: "Design",
-    image: "https://picsum.photos/300/150?random=4",
+    image: "https://picsum.photos/300/150?random=4"
   },
   {
     id: 5,
     title: "DIY and Crafts",
-    image: "https://picsum.photos/300/150?random=5",
+    image: "https://picsum.photos/300/150?random=5"
   },
   {
     id: 6,
     title: "Food and Drink",
-    image: "https://picsum.photos/300/150?random=6",
+    image: "https://picsum.photos/300/150?random=6"
   },
   {
     id: 7,
     title: "Home Decor",
-    image: "https://picsum.photos/300/150?random=7",
+    image: "https://picsum.photos/300/150?random=7"
   },
   {
     id: 8,
     title: "Men's Fashion",
-    image: "https://picsum.photos/300/150?random=8",
+    image: "https://picsum.photos/300/150?random=8"
   },
   {
     id: 9,
     title: "Quotes",
-    image: "https://picsum.photos/300/150?random=9",
+    image: "https://picsum.photos/300/150?random=9"
   },
   {
     id: 10,
     title: "Tattoos",
-    image: "https://picsum.photos/300/150?random=10",
+    image: "https://picsum.photos/300/150?random=10"
   },
   {
     id: 11,
     title: "Travel",
-    image: "https://picsum.photos/300/150?random=11",
+    image: "https://picsum.photos/300/150?random=11"
   },
   {
     id: 12,
     title: "Weddings",
-    image: "https://picsum.photos/300/150?random=12",
+    image: "https://picsum.photos/300/150?random=12"
   },
   {
     id: 13,
     title: "Women's Fashion",
-    image: "https://picsum.photos/300/150?random=13",
+    image: "https://picsum.photos/300/150?random=13"
   },
 ];
 
@@ -99,10 +99,7 @@ const BrowseByCategory = () => {
       </div>
       {isSeeMoreVisible && (
         <div className={styles.seeMoreButtonContainer}>
-          <SeeMoreButton
-            onClick={handleSeeMoreClick}
-            className={styles.seeMoreButton}
-          />
+          <SeeMoreButton onClick={handleSeeMoreClick} className={styles.seeMoreButton}/>
         </div>
       )}
     </>
