6e796f2071eee6756bc9c02d78fc0bf2ed57721b
meyco <meyco@users.noreply.github.com>
2025-04-29 21:10:49 +0200

    Add Images

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -11,10 +11,38 @@ export default function SimpleSlider() {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500
+
   };
   return (
     <div>
+      <section className={styles.topSection}>
+        {/* Backbround Area*/}
+        <div className={styles.backgroundContainer}>
+        {/* Put 18 images */}
+        <img src="/images/bg1.jpg" className={styles.bgImage} />
+        <img src="/images/bg2.jpg" className={styles.bgImage} />
+        <img src="/images/bg3.jpg" className={styles.bgImage} />
+        <img src="/images/bg4.jpg" className={styles.bgImage} />
+        <img src="/images/bg5.jpg" className={styles.bgImage} />
+        <img src="/images/bg6.jpg" className={styles.bgImage} />
+        <img src="/images/bg7.jpg" className={styles.bgImage} />
+        <img src="/images/bg8.jpg" className={styles.bgImage} />
+        <img src="/images/bg9.jpg" className={styles.bgImage} />
+        <img src="/images/bg10.jpg" className={styles.bgImage} />
+        <img src="/images/bg11.jpg" className={styles.bgImage} />
+        <img src="/images/bg12.jpg" className={styles.bgImage} />
+        <img src="/images/bg13.jpg" className={styles.bgImage} />
+        <img src="/images/bg14.jpg" className={styles.bgImage} />
+        <img src="/images/bg15.jpg" className={styles.bgImage} />
+        <img src="/images/bg16.jpg" className={styles.bgImage} />
+        <img src="/images/bg17.jpg" className={styles.bgImage} />
+        <img src="/images/bg18.jpg" className={styles.bgImage} />
+    </div>
+
+      {/*  Text Slider */}
       <p className={styles.label}>Get your next</p>
       <Slider {...settings}>
         <div>
@@ -30,6 +58,7 @@ export default function SimpleSlider() {
           <p className={`${styles.slideText} ${styles.Text4}`}>green thumb idea</p>
         </div>
       </Slider>
+      </section>
     </div>
   );
 }
