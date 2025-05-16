f6e1bd122c95bb719b730e7beef6dba9a3f4a74d
meistergpc <jap1centeno@gmail.com>
2025-05-16 23:43:51 +0200

    testing get

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,7 +5,7 @@ import {
   BAD_REQUEST,
   CREATED,
   INTERNAL_SERVER_ERROR,
-  OK
+  OK,
 } from "../constants/http.js";
 
 export const signup = async (req, res) => {
@@ -26,7 +26,7 @@ export const signup = async (req, res) => {
     const cycles = 10;
     const hashedPassword = await bcrypt.hash(password, cycles);
     const newUser = await prisma.user.create({
-      data: { email, password: hashedPassword, name }
+      data: { email, password: hashedPassword, name },
     });
 
     res
@@ -61,12 +61,33 @@ export const login = async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "7d"
+      expiresIn: "7d",
     });
 
     res.status(OK).json({ message: "Login successfull!", token });
   } catch (error) {
     console.error("Error is login controller", error.message);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal Server Error" });
+  }
+};
+
+// test get
+export const getUserHandler = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        createdAt: true,
+      },
+    });
+
+    res.status(OK).json({ users });
+  } catch (error) {
+    console.error("Error in getUsers controller", error);
     res
       .status(INTERNAL_SERVER_ERROR)
       .json({ message: "Internal Server Error" });
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { signup, login } from "../controllers/userController.js";
+import {
+  signup,
+  login,
+  getUserHandler,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+// test get
+router.get("/users", getUserHandler);
 
 export default router;
