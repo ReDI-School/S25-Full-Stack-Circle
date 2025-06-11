4b494a744bf99c29f9190eb9ebe6b2d4afa92e56
NavyaBharathan <navyabmanikkoth@live.com>
2025-06-11 12:41:45 +0200

    Fixed eslint

--- frontend/src/components/NavbarLoggedIn/NavbarLoggedIn.jsx
+++ frontend/src/components/NavbarLoggedIn/NavbarLoggedIn.jsx
@@ -5,25 +5,23 @@ import { Input } from "@mui/material";
 
 import { DropdownProfile, DropdownFilter } from "../Dropdown/Dropdown";
 
-const NavbarLoggedIn = () => {
-  return (
-    <div>
-      <div className={styles["searchbar-profile"]}>
-        <div className={styles["searchbar-wrapper"]}>
-          <SearchIcon />
-          <Input
-            className={styles["searchbar-input"]}
-            type="text"
-            placeholder="Search Your Pins"
-            disableUnderline
-          />
-          <DropdownFilter />
-        </div>
-        <div className={styles["profile-logo"]}>N</div>
-        <DropdownProfile />
+const NavbarLoggedIn = () => (
+  <div>
+    <div className={styles["searchbar-profile"]}>
+      <div className={styles["searchbar-wrapper"]}>
+        <SearchIcon />
+        <Input
+          className={styles["searchbar-input"]}
+          type="text"
+          placeholder="Search Your Pins"
+          disableUnderline
+        />
+        <DropdownFilter />
       </div>
+      <div className={styles["profile-logo"]}>N</div>
+      <DropdownProfile />
     </div>
-  );
-};
+  </div>
+);
 
 export default NavbarLoggedIn;
--- frontend/src/components/ProfileInfo/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo/ProfileInfo.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import styles from "./ProfileInfo.module.css";
 import { FiCheck } from "react-icons/fi";
 
-const ProfileInfo = () => {
-  return (
-    <div className={styles["ProfileInfo-wrapper"]}>
-      <div className={styles["ProfileInfo-image"]}></div>
-      <div className={styles["ProfileInfo-text"]}>
-        <div className={styles["ProfileInfo-username"]}>User's Name</div>
-        <div className={styles["ProfileInfo-email"]}>User's Email Id</div>
-        <div className={styles["ProfileInfo-followers"]}>10 Followers</div>
-      </div>
-      <div>
-        <FiCheck />
-      </div>
+const ProfileInfo = () => (
+  <div className={styles["ProfileInfo-wrapper"]}>
+    <div className={styles["ProfileInfo-image"]}></div>
+    <div className={styles["ProfileInfo-text"]}>
+      <div className={styles["ProfileInfo-username"]}>User's Name</div>
+      <div className={styles["ProfileInfo-email"]}>User's Email Id</div>
+      <div className={styles["ProfileInfo-followers"]}>10 Followers</div>
     </div>
-  );
-};
+    <div>
+      <FiCheck />
+    </div>
+  </div>
+);
 
 export default ProfileInfo;
