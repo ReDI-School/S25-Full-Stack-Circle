7af3822b1ddd0d3d27fde141900255cfbce6e420
NavyaBharathan <navyabmanikkoth@live.com>
2025-06-16 16:05:01 +0200

    display users pin

--- frontend/src/pages/UserPins/UserPins.jsx
+++ frontend/src/pages/UserPins/UserPins.jsx
@@ -4,37 +4,11 @@ import ProfileInfo from "../../components/ProfileInfo/ProfileInfo";
 
 const UserPins = () => {
   //{ user, createdPins, savedPins }
-  const [activeTab, setActiveTab] = useState("created");
+  const [activeTab, setActiveTab] = useState("mypins");
   const [createdPins, setCreatedPins] = useState([]);
   const [savedPins, setSavedPins] = useState([]);
   const token = localStorage.getItem("authToken");
 
-  /*const savedPins = [
-    {
-      id: 5,
-      title: "Sunset City",
-      imageUrl:
-        "https://images.unsplash.com/photo-1414609245224-afa02bfb3fda?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      id: 6,
-      title: "Forest Trail",
-      imageUrl:
-        "https://images.unsplash.com/photo-1503435824048-a799a3a84bf7?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZvcmVzdHxlbnwwfDF8MHx8fDI%3D"
-    },
-    {
-      id: 7,
-      title: "Sunset City",
-      imageUrl:
-        "https://images.unsplash.com/photo-1517685633466-403d6955aeab?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3Vuc2V0fGVufDB8MXwwfHx8Mg%3D%3D"
-    },
-    {
-      id: 8,
-      title: "Forest Trail",
-      imageUrl:
-        "https://images.unsplash.com/photo-1444724334165-e7050f2229a1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcmVzdHxlbnwwfDF8MHx8fDI%3D"
-    }
-  ];*/
   const getDisplayPins = () => {
     if (activeTab === "created") return createdPins;
     if (activeTab === "saved") return savedPins;
@@ -98,14 +72,14 @@ const UserPins = () => {
       console.error("Error in fetching the My Pins");
     }
   };
-
   useEffect(() => {
-    if (activeTab === "created") {
+    if (activeTab === "created" && createdPins.length === 0) {
       getCreatedPins();
-    } else if (activeTab === "saved") {
+    } else if (activeTab === "saved" && savedPins.length === 0) {
       getSavedPins();
-    } else {
-      getMyPins();
+    } else if (activeTab === "mypins") {
+      if (createdPins.length === 0) getCreatedPins();
+      if (savedPins.length === 0) getSavedPins();
     }
   }, [activeTab]);
 
