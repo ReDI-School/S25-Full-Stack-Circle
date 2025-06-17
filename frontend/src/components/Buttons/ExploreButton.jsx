46c562f1b9256c0313d47fb1d2b9d650b8c11131
NavyaBharathan <navyabmanikkoth@live.com>
2025-06-17 18:57:42 +0200

    added Explore button

--- frontend/src/components/Buttons/ExploreButton.jsx
+++ frontend/src/components/Buttons/ExploreButton.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./Explore.module.css";
+import { useNavigate } from "react-router-dom";
 
 const ExploreButton = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <button className={styles.explore_button}>Explore</button>
+      <button
+        className={styles.explore_button}
+        onClick={() => navigate("/explore")}
+      >
+        Explore
+      </button>
     </div>
   );
 };
